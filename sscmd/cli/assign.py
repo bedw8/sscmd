dda8e7fa227515825bdc6d3518b75d484a2ddb29
Benjamin <bedw@bedw.me>
2022-11-03 17:13:49 -0300

    assign cli improvement

--- sscmd/cli/assign.py
+++ sscmd/cli/assign.py
@@ -10,6 +10,7 @@ from .options import OutputFileOption, OutputFileArg, ForceOverwriteOption, ReqA
 from ..assignments import AssignmentsApi
 from copy import deepcopy
 from ast import literal_eval as le
+from rich.progress import track
 
 app = typer.Typer()
 
@@ -26,16 +27,17 @@ def main(ctx: typer.Context):
 def get_list(ctx: typer.Context, 
         file: Path = OutputFileArg('la lista de asignaciones',...),
         force: Optional[bool] = ForceOverwriteOption(),
-        questionnaire_id: Optional[str] = typer.Option(None,'--id',help='Identificador del cuestionario',show_default=False),
-        questionnaire_version: Optional[int] = typer.Option(None,'--version',help='Versión del cuestionario',show_default=False),
-        order: Optional[str] = typer.Option(None,'--order',help='Variable según la cual ordenar los resultados'),
-        id_range: Optional[str] = typer.Option(None,'--range',help="Especifica el rango de Ids"), 
+        questionnaire_id: Optional[str] = typer.Option(None,'--id','-q',help='Identificador del cuestionario',show_default=False),
+        questionnaire_version: Optional[int] = typer.Option(None,'--version','-v',help='Versión del cuestionario',show_default=False),
+        order: Optional[str] = typer.Option(None,'--sort','-s',help='Variable según la cual ordenar los resultados',show_default=False),
+        id_range: Optional[str] = typer.Option(None,'--range','-r',help="Especifica el rango de Ids",show_default=False), 
         query: Optional[str] = typer.Option(None,'--query',help='Query de busqueda',show_default=False),
-        folios: Optional[bool] = typer.Option(False,'--folio',help='Incluir folios (más lento)')
+        folios: Optional[bool] = typer.Option(False,'--folios',help='Consultar folios las solicitudes (más lento)'),
+        add_folios: Optional[Path] = typer.Option(None,'--folios-df',help='Añadar folios (merge) previamente obtenidos',show_default=False)
         ):
     check_path_to_write(file,force) 
     
-    api_params = {key: ctx.params[key] for key in ctx.params if key not in ['file','force']}
+    api_params = {key: ctx.params[key] for key in ctx.params if key not in ['file','force','folios','add_folios']}
 
     if id_range:
         api_params['id_range'] = le(api_params['id_range'])
@@ -46,16 +48,25 @@ def get_list(ctx: typer.Context,
     if not questionnaire_version:
         api_params['questionnaire_version'] = ctx.parent.parent.config['general']['q_version']
 
+    total = (api_params['id_range'][1] - api_params['id_range'][0])
+
+    assigns = []
     api = AssignmentsApi(ctx.parent.parent.client)
-    assigns = list(api.get_list(**api_params))
+    list_generator = api.get_list(**api_params)
+    progress = track(list_generator,total=total) if id_range else list_generator  
+    for a in progress:
+        assigns.append(a)
     df = pd.DataFrame(assigns)
     df = df
 
-    if not file:
-        print(df_to_table(df))
-    else:
-        df.to_csv(file,index=False)
-        print('Se escribío el archivo',file)
+    if add_folios:
+        folios_df = pd.read_csv(add_folios)
+        df = df.merge(folios_df,on='Id',how='left')
+        print('Se añadieron folios')
+
+    df.to_csv(file,index=False)
+    print('Se escribío el archivo',file)
+
 
 @app.command('details')
 @docs_from(AssignmentsApi.get_id_details)
